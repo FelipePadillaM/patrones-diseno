0dc3d36f3734c44007a3717c504f213fed3df3f7
Andres Padilla <90018159+FelipePadillaM@users.noreply.github.com>
2025-05-05 22:18:07 -0500

    20250505: abstract factory.

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Abstract Factory:
  * Es un patrón de diseño que permite crear familias de objetos relacionados
@@ -17,3 +18,77 @@
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburguer {
+    prepare(): void;
+}
+
+interface Drink {
+    pour(): void;
+}
+
+class ChickenHamburger implements Hamburguer {
+    prepare(): void {
+        console.log("Preparing %cChicken Hamburger...", COLORS.yellow);
+    }
+}
+
+class BeefHamburger implements Hamburguer {
+    prepare(): void {
+        console.log("Preparing %cBeef Hamburger...", COLORS.red);
+    }
+}
+
+class Water implements Drink {
+    pour(): void {
+        console.log("Pouring %cWater...", COLORS.blue);
+    }
+}
+
+
+class Soda implements Drink {
+    pour(): void {
+        console.log("Pouring %cSoda...", COLORS.green);
+    }
+}
+
+
+interface RestaurantFactory {
+    createHamburger(): Hamburguer;
+    createDrink(): Drink;
+}
+
+class FastFoodRestaurant implements RestaurantFactory {
+    createHamburger(): Hamburguer {
+        return new BeefHamburger();
+    }
+
+    createDrink(): Drink {
+        return new Soda();
+    }
+}
+
+class HealthyRestaurant implements RestaurantFactory {
+    createHamburger(): Hamburguer {
+        return new ChickenHamburger();
+    }
+
+    createDrink(): Drink {
+        return new Water();
+    }
+}
+
+function main(factory: RestaurantFactory) {
+    const hamburger = factory.createHamburger();
+    const drink = factory.createDrink();
+
+    hamburger.prepare();
+    drink.pour();
+}
+
+
+console.log("%cAbstract Factory", COLORS.purple);
+main(new FastFoodRestaurant()); // Fast food restaurant
+
+console.log("\n%cAbstract Factory", COLORS.purple);
+main(new HealthyRestaurant()); // Healthy restaurant
